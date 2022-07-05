e3db54c258a9cef91d18569f322aff0031732bd4
Coelacanth101 <106444731+Coelacanth101@users.noreply.github.com>
2022-07-05 20:09:03 +0900

    'commit'

---
+++ serve.js
@@ -0,0 +1,28 @@
+const { Console } = require("console");
+const { emit } = require("process");
+
+//サーバー用変数
+const app  = require("express")();
+const http = require("http").createServer(app);
+const io   = require("socket.io")(http);
+const DOCUMENT_ROOT = __dirname + "/static";
+const SECRET_TOKEN = "abcdefghijklmn12345";
+
+app.get("/", (req, res)=>{
+  res.sendFile(DOCUMENT_ROOT + "/index.html");
+});
+
+app.get("/:file", (req, res)=>{
+  res.sendFile(DOCUMENT_ROOT + "/" + req.params.file);
+});
+
+/**
+ * 3000番でサーバを起動する
+ */
+ http.listen(process.env.PORT || 3000, ()=>{
+  console.log("listening on *:3000");
+});
+
+
+/*------------------------------------------
+------------------------------------------*/
